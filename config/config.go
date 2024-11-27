ead62c90904200229b471b608e585e250844356e
Yadnesh Kulkarni <ykulkarn@redhat.com>
2024-11-27 13:33:22 +0530

    Add support for setting configuration with env variables

--- config/config.go
+++ config/config.go
@@ -17,15 +17,16 @@ package config
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"time"
+
+	"log/slog"
 
 	"gopkg.in/yaml.v3"
 )
 
 var (
-	configPath = "/etc/metricly/config.yaml"
+	configPathDefault = "/etc/metricly/config.yaml"
 )
 
 type Config struct {
@@ -38,12 +39,19 @@ type Config struct {
 		Port    string `yaml:"port"`
 	} `yaml:"prometheus"`
 	CollectionInterval time.Duration `yaml:"interval"`
+	Debug              bool          `yaml:"debug"`
 }
 
-func LoadConfig() (*Config, error) {
-	conf, err := os.Open(configPath)
+func LoadConfig(configPath *string) (*Config, error) {
+	if *configPath == "" {
+		// if config file is not passed in cli, parse configs from configPathDefault
+		configPath = &configPathDefault
+	}
+
+	// attempt to load config
+	conf, err := os.Open(*configPath)
 	if err != nil {
-		log.Fatalf("failed to open config file from default location: %s\n", configPath)
+		return nil, fmt.Errorf("failed to open config file from default location: %s", *configPath)
 	}
 	defer conf.Close()
 
@@ -52,6 +60,47 @@ func LoadConfig() (*Config, error) {
 	if err := decoder.Decode(&cfg); err != nil {
 		return nil, fmt.Errorf("failed to parse config file %v", err)
 	}
-	log.Println("config loaded successfully: ", configPath)
+	slog.Info(fmt.Sprintf("Config loaded successfully from %s", *configPath))
+
+	// checking if any variable was overrided through environment variables
+	if env := os.Getenv("SERVER_ADDRESS"); env != "" {
+		cfg.Server.Address = env
+	}
+	if env := os.Getenv("SERVER_PORT"); env != "" {
+		cfg.Server.Port = env
+	}
+	if env := os.Getenv("PROMETHEUS_ADDRESS"); env != "" {
+		cfg.Prometheus.Address = env
+	}
+	if env := os.Getenv("PROMETHEUS_PORT"); env != "" {
+		cfg.Prometheus.Port = env
+	}
+	if env := os.Getenv("COLLECTION_INTERVAL"); env != "" {
+		if interval, err := time.ParseDuration(env); err == nil {
+			cfg.CollectionInterval = interval
+		} else {
+			return nil, fmt.Errorf("invalid COLLECTION_INTERVAL value: %v", err)
+		}
+	}
+	if env := os.Getenv("DEBUG"); env != "" {
+		if debug, err := parseBool(env); err == nil {
+			cfg.Debug = debug
+		} else {
+			return nil, fmt.Errorf("invalid DEBUG value: %v", err)
+		}
+	}
+
 	return &cfg, nil
+}
+
+// parseBool parses a string into a boolean value.
+func parseBool(value string) (bool, error) {
+	switch value {
+	case "1", "true", "TRUE", "True":
+		return true, nil
+	case "0", "false", "FALSE", "False":
+		return false, nil
+	default:
+		return false, fmt.Errorf("invalid boolean value: %s", value)
+	}
 }
